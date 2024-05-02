063b007d5a74c3537c2002953940fddb4ebc1e2f
Shub017 <ushubham91@gmail.com>
2024-05-02 13:36:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css'
+import { useContext, useEffect } from 'react';
+import { CustomContext } from './CustomContext/customContext';
 
 function App() {
+
+  const {arecotentsFetched, Fetchedcontents, fetchItemsOfList, addNewResource, setTitle, setContent, title, content, titleInputRef, delteItem, updateItem, isModalOpen, setIsModalOpen, selectedItem, setSelectedItem, setId} = useContext(CustomContext);
+  
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await fetchItemsOfList();
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+  
+    fetchData();
+    
+  }, []);
+
+  const submitForm = async (event)=>{
+    event.preventDefault();
+    await addNewResource()
+    setTitle((preval)=> '');
+    setContent((preval)=> '');
+    setId((preval)=> preval+1);
+    if (titleInputRef.current) {
+      titleInputRef.current.focus();
+    }
+  }
+
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleUpdate = (item) => {
+    console.log('handle Updated clicked', item);
+    setSelectedItem(item);
+    setTitle(item.title);
+    setContent(item.body);
+    openModal();
+  };
+
+  const submitUpadteForm = async (event) =>{
+    event.preventDefault();
+    await updateItem({id:selectedItem.id, userId:selectedItem.userId});
+    setSelectedItem(null);
+    setTitle('');
+    setContent('');
+    closeModal();
+    } 
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className={`${styles.ListContainer} `}>
+      <span className={styles.heading}>𝕿𝖔𝖉𝖔𝕷𝖎𝖘𝖙</span>
+
+      <div className={`${styles.Form}`} >
+        <form id="commentForm" className={styles.formBorder} onSubmit={submitForm}>
+          <div>
+              <label for="title" className={styles.FormTitle}>Title :</label>
+              <input type="text" id="title" name="title" value={title} onChange={(e) => setTitle(e.target.value)} required className={styles.formTitleInput} ref={titleInputRef}/>
+          </div>
+          <div>
+              <label for="content" className={styles.formContentLabel}>Content :</label>
+              <textarea id="content" name="content" rows="4" value={content} onChange = {(e)=>setContent(e.target.value)} required className={styles.formContentInput}></textarea>
+          </div>
+          <button type="submit" className={`${styles.customBtn} ${styles.btn16}`} >Submit</button>
+        </form>
+      </div>
+
+      {/* Displaying fetched list */}
+      {arecotentsFetched? Fetchedcontents.map((d)=>{
+        return(<div className={styles.ListContainerContent} key={d.id}>
+          <div className={styles.idUidContent}>
+            <span className={styles.idUid}>Id: {d.id}</span>
+            <span className={styles.idUid}>Uid: {d.userId}</span>
+          </div>
+
+          <p className={styles.listTitle}>Title: {d.title}</p>
+          <div className={styles.listContent}>{d.body}</div>
+          <div className={styles.buttonContainer}>
+          <button class={`${styles.customBtn2} ${styles.btn12}`} onClick={()=> {delteItem({ id: d.id, userId: d.userId })}}><span>Click!</span><span>Delete</span></button> 
+          <button class={`${styles.customBtn2} ${styles.btn13}`} onClick={()=> {handleUpdate(d)}}><span>Click!</span><span>Update</span></button> 
+          </div>
+        </div>)
+      }):''}
+
+
+      
+    
     </div>
+    
+
+    {isModalOpen && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <span className={styles.close} onClick={closeModal}>&times;</span>
+            <h2>Edit Item</h2>
+            <form onSubmit={submitUpadteForm}>
+              <label htmlFor="modal-title" className={styles.modalTitleLabel}>Title:</label>
+              <input type="text" id="modal-title" value={title} className={styles.modalTitle} onChange={(e) => setTitle(e.target.value)} required />
+              <label htmlFor="modal-content" className={styles.modalTitleLabel}>Content:</label>
+              <textarea id="modal-content" value={content} className={styles.modalBody} onChange={(e) => setContent(e.target.value)} required />
+              <button type="submit" className={styles.editSubmitButton}>Update</button>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/CustomContext/customContext.js
@@ -0,0 +1,114 @@
+import { createContext, useState, useRef } from "react";
+
+
+
+const CustomContext = createContext();
+
+export default function CustomProviderContext({children}){
+
+    const [arecotentsFetched, setAreContentsFetched] = useState(false);
+    const [Fetchedcontents, setFetchedContents] = useState([]);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [id, setId] = useState(1);
+    const titleInputRef = useRef(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const fetchItemsOfList = ()=>{
+        try{fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((json) => {
+            setFetchedContents((prevVal)=>{
+                if(Array.isArray(json)){
+                    return json;
+                }
+                else{
+                    return [json];
+                }
+            });
+            setAreContentsFetched((prevVal)=>{return true});
+            console.log(json);
+        });}
+        catch(error){
+            console.log(error);
+            setAreContentsFetched((prevVal)=>{return false});
+        }
+    }
+
+    const addNewResource = async ()=>{
+        if (title.trim() === '' || content.trim() === ''){
+            console.log('Empty fields not allowed');
+            return;
+        }
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: title,
+                body: content,
+                userId: id,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        
+        const newResource = await response.json();
+        console.log(newResource); // Here you can access the data from the response
+        setFetchedContents((prevVal)=>{return [ newResource, ...prevVal]});
+    }
+
+    const delteItem = async (props)=>{
+        const {id, userId} = props;
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'DELETE',
+        });
+
+        
+        
+        const tempContent = Fetchedcontents.filter((d) => {
+            console.log(`Checking ${d.id} ${d.userId} compared with ${id} ${userId}`)
+            return d.id !== id || d.userId !== userId;
+        });
+        console.log('Contents after deletion', tempContent);
+        setFetchedContents(()=> tempContent);
+        
+    }
+
+    const updateItem = async ({id, userId})=> {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PUT',
+            body: JSON.stringify({
+                id: id,
+                title: title,
+                body: content,
+                userId: userId,
+            }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+
+        const newResource = await response.json();
+        console.log(newResource);
+        const newFetchedContent = Fetchedcontents.map((d)=>{
+            if (d.id === id && d.userId === userId){
+                return newResource;
+            }
+            else{
+                return d;
+            }
+        })
+
+        setFetchedContents(() => newFetchedContent);
+    }
+
+
+    return(
+        <CustomContext.Provider value={{arecotentsFetched, Fetchedcontents, fetchItemsOfList, addNewResource, title, content, id, setTitle, setContent, titleInputRef, delteItem, updateItem, isModalOpen, setIsModalOpen, selectedItem, setSelectedItem, setId}}>
+            {children}
+        </CustomContext.Provider>
+    )
+}
+
+export {CustomContext};
